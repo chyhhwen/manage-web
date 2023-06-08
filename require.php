61219c02f252aadb1f7f100904ee07763b1cd175
chyhhwen <chyhhwen@gmail.com>
2023-06-08 08:54:29 +0800

    test

---
+++ require.php
@@ -0,0 +1,16 @@
+<?php
+    return [
+        "db" => "manage",
+        "table" => "user",
+        "table1" => "access",
+        "table2" => "list",
+        "table3" => "memo",
+        "user" => "root",
+        "pass" => "",
+        "na" => "name",
+        "un" => "username",
+        "pa" => "password",
+        "we" => "web",
+        "th" => "thing"
+      ];
+?>
